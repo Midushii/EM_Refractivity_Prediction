ac8b5f1bd74eba1c5d46c12f86c04602a6240605
Midushi Maheshwari <midushi.maheswari@gmail.com>
2026-08-20 18:21:12 +0530

    Update 07_Predict_2026.py

--- Python_Codes/07_Predict_2026.py
+++ Python_Codes/07_Predict_2026.py
@@ -1,40 +1,14 @@
-"""
-=========================================================
-07_Predict_2026.py
-
-Purpose:
-Predict Radio Refractivity for 2026 using the
-trained Random Forest model.
-
-Inputs:
-    RandomForest_Model.pkl
-    ML_Features_2026.csv
-
-Outputs:
-    Predicted_2026.csv
-=========================================================
-"""
-
 import pandas as pd
 import joblib
 
 print("="*60)
 print("PREDICTING RADIO REFRACTIVITY FOR 2026")
 print("="*60)
-
-# ----------------------------------------------------
-# Load Model
-# ----------------------------------------------------
-
 print("\nLoading Random Forest Model...")
 
 model = joblib.load("RandomForest_Model.pkl")
 
 print("Model Loaded Successfully")
-
-# ----------------------------------------------------
-# Read Dataset
-# ----------------------------------------------------
 
 print("\nReading ML_Features_2026.csv...")
 
@@ -42,10 +16,6 @@ df = pd.read_csv("ML_Features_2026.csv")
 
 print("Dataset Loaded")
 print(df.shape)
-
-# ----------------------------------------------------
-# Features (Must match training exactly)
-# ----------------------------------------------------
 
 features = [
 
@@ -76,19 +46,11 @@ print("\nUsing Features:")
 for feature in features:
     print(" •", feature)
 
-# ----------------------------------------------------
-# Prediction
-# ----------------------------------------------------
-
 print("\nPredicting...")
 
 prediction = model.predict(df[features])
 
 df["Predicted_Refractivity"] = prediction
-
-# ----------------------------------------------------
-# Save
-# ----------------------------------------------------
 
 df.to_csv(
 
